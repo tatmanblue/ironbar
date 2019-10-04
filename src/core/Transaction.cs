79c43db576f2fb6ddae25cd9c8c89e7971a32ce4
Matt Raffel <mattr@kiva.org>
2019-10-04 13:53:51 -0700

    NOT OFFICIAL code, just proof we have a blockchain

---
+++ src/core/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace core
+{
+    public class Transaction
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
