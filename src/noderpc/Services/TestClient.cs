349c88d5cbc59da936d6ff44e6bb3f59d01054c8
Matt Raffel <mattr@kiva.org>
2019-10-28 16:47:30 -0600

    no message

---
+++ src/noderpc/Services/TestClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace noderpc
+{
+    public class TestClient
+    {
+        private readonly Options _options;
+        public TestClient(Options options)
+        {
+            _options = options;
+        }
+
+        public async Task ConnectToBootNode()
+        {
+            var channel = GrpcChannel.ForAddress($"https://localhost:{_options.ServerRPCPort}");
+            var client = new boot_node.BootNodeClient(channel);
+            var reply = await client.AddLink(
+                              new LinkRequest { ClientPort = "1" });
+            Console.WriteLine("BootNode says: " + reply.Message);
+        }
+    }
+}
