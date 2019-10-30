29edb19db1132f3082a730211c77942d6c6d872e
tatmanblue <matt.raffel@gmail.com>
2019-10-29 19:11:21 -0600

    adding controller
    Signed-off-by: tatmanblue <matt.raffel@gmail.com>

---
+++ src/noderpc/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Core.DTO;
+
+namespace noderpc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+
+        private readonly ILogger<AdminController> logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ConnectedNode> Get()
+        {
+            List<ConnectedNode> nodes = new List<ConnectedNode>();
+            nodes.Add(new ConnectedNode() { Name = "test1" });
+        }
+    }
+}
