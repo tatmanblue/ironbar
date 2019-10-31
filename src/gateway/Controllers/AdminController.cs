92295a905b901534f5161cca649bf2823988b91d
Matt Raffel <mattr@kiva.org>
2019-10-31 13:43:14 -0600

    adding a gateway which will handle webapi for now

---
+++ src/gateway/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace noderpc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+
+        private readonly ILogger<AdminController> logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            this.logger = logger;
+        }
+
+   //     [HttpGet]
+   //     public IEnumerable<ConnectedNode> Get()
+   //     {
+   //         List<ConnectedNode> nodes = new List<ConnectedNode>();
+   //         nodes.Add(new ConnectedNode() { Name = "test1" });
+			//return nodes;
+   //     }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "admin hello";
+        }
+    }
+}
