4077862aa9f18793d7b3e43dcaea8764f82bd549
Matt Raffel <mattr@kiva.org>
2019-12-11 11:26:26 -0800

    updating interface

--- src/core/plugin/IPlugin.cs
+++ src/core/plugin/IPlugin.cs
@@ -5,5 +5,10 @@
 public interface IPlugin
 {
     void Init();
+    // must be in the format of "major.minor.patch.build"
+    // to be accepted in release builds nothing starting with "0" will be allowed
     string Version();
+    // must be an id that can be referenced by consumers.
+    // TODO: not sure yet what to do in the event of id collisions
+    string Id();
 }
