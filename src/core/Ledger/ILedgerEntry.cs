863557aa9d231b9a8d0cc7f27de34dc353b15111
Matt Raffel <mattr@kiva.org>
2019-12-13 09:55:28 -0800

    adding interfaces
    Signed-off-by: Matt Raffel <mattr@kiva.org>

---
+++ src/core/Ledger/ILedgerEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace core.Ledger
+{
+    /// <summary>
+    /// this represents data as it will appear when saved on disk
+    /// </summary>
+    public interface ILedgerPhysicalBlock
+    {
+        int LedgerId { get; }
+        DateTime TimeStamp { get; }
+        long Nonce { get; }
+        string PreviousHash { get; }
+        string Hash { get; }
+        byte[] TransactionData { get; }
+
+        string ComputeHash();
+    }
+
+    /// <summary>
+    /// This represents a block in memory 
+    /// </summary>
+    public interface ILedgerLogicalBlock<T>
+    {
+        int LedgerId { get; }
+        DateTime TimeStamp { get; }
+        long Nonce { get; }
+        string PreviousHash { get; }
+        string Hash { get; }
+        List<T> TransactionData { get; }
+
+        string ComputeHash();
+    }
+}
