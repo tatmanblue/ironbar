902e33cface7a12c48b54a04df268337df282c52
Matt Raffel <mattr@kiva.org>
2020-01-31 15:28:23 -0800

    fixes path
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- scripts/start_master_node.sh
+++ scripts/start_master_node.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ROOT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 mkdir -p data
-dotnet run --project ../src/node/ -- -d "$ROOT_PATH/data"
+echo "${ROOT_PATH}data"
+dotnet run --project ../src/node/ -- -d "${ROOT_PATH}data"
