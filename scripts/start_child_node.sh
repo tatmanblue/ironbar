3ef80d1bd4510f4e7bda0675a3ce40bd8c713ccf
Matt Raffel <matt.raffel@gmail.com>
2025-08-11 16:21:47 -0600

    idk maybe
    Signed-off-by: Matt Raffel <matt.raffel@gmail.com>

--- scripts/start_child_node.sh
+++ scripts/start_child_node.sh
@@ -1,6 +1,6 @@
-IRONBAR_BOOT_SERVER=http://localhost:50051
-IRONBAR_DATA_PATH=f:\temp\ironbar
-IRONBAR_RPC_PORT=50055
-IRONBAR_TYPE=child
+export IRONBAR_BOOT_SERVER=http://localhost:50051
+export IRONBAR_DATA_PATH=f:\temp\ironbar
+export IRONBAR_RPC_PORT=50055
+export IRONBAR_TYPE=child
 ECHO Starting child node....see output for configuration 
 dotnet run --project ../src/node/
